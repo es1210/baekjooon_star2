8968fb88ea4d1828a8119cd4a7a1628f1b056b13
es1210 <dmstn27034@gmail.com>
2024-06-23 20:17:07 +0900

    solve-2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,20 @@
 package org.example;
 
-import java.util.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(Arrays.toString(Solution.solution(10, 3)));
+        int N = sc.nextInt();
 
-    }
-}
-
-class Solution {
-    public static int[] solution(int l, int r) {
-        int[] answer = new int[n / k];
-
-        for(; l <= r; l++) {
-            if(l == )
-
+        for(int i = 1; i <= N; i++) {		// 행 반복
+            for(int j = 1; j <= N - i; j++){	// 공백 반복
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= i; k++){	// 별 반복
+                System.out.print("*");
+            }
+            System.out.println();		// 줄 바꿈
         }
-        return answer;
     }
 }
