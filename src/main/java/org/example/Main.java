dd54d9a721be56df7f9ca3dd970c6a6b3a1400f6
es1210 <dmstn27034@gmail.com>
2024-06-23 19:49:14 +0900

    solve-1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(Solution.solution(10, 3)));
+
+    }
+}
+
+class Solution {
+    public static int[] solution(int l, int r) {
+        int[] answer = new int[n / k];
+
+        for(; l <= r; l++) {
+            if(l == )
+
+        }
+        return answer;
+    }
+}
